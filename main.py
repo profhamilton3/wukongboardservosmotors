6c7325297c9f80e7537f7c69d93c65af73c75100
T. Hamilton <profhamilton3@gmail.com>
2023-05-13 19:43:59 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 def on_button_pressed_a():
+    basic.show_icon(IconNames.CHESSBOARD)
     wuKong.set_motor_speed(wuKong.MotorList.M1, 52)
-    wuKong.set_motor_speed(wuKong.MotorList.M1, 0)
+    wuKong.set_servo_speed(wuKong.ServoList.S0, 39)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     wuKong.stop_all_motor()
-    wuKong.set_motor_speed(wuKong.MotorList.M1, 0)
+    basic.show_icon(IconNames.BUTTERFLY)
+    wuKong.set_servo_speed(wuKong.ServoList.S0, 0)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 wuKong.set_light_mode(wuKong.LightMode.BREATH)
+basic.show_leds("""
+    # # # . .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+""")
 
 def on_forever():
-    basic.show_icon(IconNames.HEART, 750)
+    basic.show_icon(IconNames.SMALL_DIAMOND, 750)
     basic.show_icon(IconNames.SMALL_HEART, 500)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showIcon(IconNames.Chessboard)
     wuKong.setMotorSpeed(wuKong.MotorList.M1, 52)
-    wuKong.setMotorSpeed(wuKong.MotorList.M1, 0)
+    wuKong.setServoSpeed(wuKong.ServoList.S0, 39)
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     wuKong.stopAllMotor()
-    wuKong.setMotorSpeed(wuKong.MotorList.M1, 0)
+    basic.showIcon(IconNames.Butterfly)
+    wuKong.setServoSpeed(wuKong.ServoList.S0, 0)
 })
 wuKong.setLightMode(wuKong.LightMode.BREATH)
+basic.showLeds(`
+    # # # . .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+`)
 basic.forever(function on_forever() {
-    basic.showIcon(IconNames.Heart, 750)
+    basic.showIcon(IconNames.SmallDiamond, 750)
     basic.showIcon(IconNames.SmallHeart, 500)
 })
